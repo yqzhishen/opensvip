96e039d277e679ee289c4a68a14e483bf087f193
yqzhishen <yangqian_1015@icloud.com>
2022-03-11 15:29:48 +0800

    C# implementation

---
+++ csharp/Console/Console.cs
@@ -0,0 +1,15 @@
+﻿using OpenSvip.Stream;
+
+namespace OpenSvip.Console;
+
+internal static class ConsoleApp
+{
+    public static void Main(string[] args)
+    {
+        var project = Binary.Read(@"E:\YQ数据空间\YQ实验室\实验室：XStudioSinger\Dev Projects\opensvip\test\黏黏黏黏.svip");
+        Json.Dump(@"E:\YQ数据空间\YQ实验室\实验室：XStudioSinger\Dev Projects\opensvip\test\csharp.json", project, indented: true);
+        project = Json.Load(@"E:\YQ数据空间\YQ实验室\实验室：XStudioSinger\Dev Projects\opensvip\test\csharp.json");
+        Binary.Write(@"E:\YQ数据空间\YQ实验室\实验室：XStudioSinger\Dev Projects\opensvip\test\repack.svip", project);
+        Json.Dump(@"E:\YQ数据空间\YQ实验室\实验室：XStudioSinger\Dev Projects\opensvip\test\repack.json", project, indented: true);
+    }
+}
---
+++ csharp/Stream/Stream.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using Newtonsoft.Json;
+using OpenSvip.Model;
+
+namespace OpenSvip.Stream;
+
+public static class Binary
+{
+    public static Project Read(string path)
+    {
+        var reader = new FileStream(path, FileMode.Open, FileAccess.Read);
+        var buffer = new byte[9];
+        reader.ReadByte();
+        reader.Read(buffer, 0, 4);
+        reader.ReadByte();
+        reader.Read(buffer, 4, 5);
+        var version = Encoding.Default.GetString(buffer);
+        var model = (SingingTool.Model.AppModel) new BinaryFormatter().Deserialize(reader);
+        reader.Close();
+        reader.Dispose();
+        return new Project().Decode(version, model);
+    }
+
+    public static void Write(string path, Project project)
+    {
+        var (version, model) = project.Encode();
+        var buffer = Encoding.Default.GetBytes(version);
+        var writer = new FileStream(path, FileMode.Create, FileAccess.Write);
+        writer.WriteByte(4);
+        writer.Write(buffer, 0, 4);
+        writer.WriteByte(5);
+        writer.Write(buffer, 4, 5);
+        new BinaryFormatter().Serialize(writer, model);
+        writer.Flush();
+        writer.Close();
+        writer.Dispose();
+    }
+}
+
+public static class Json
+{
+    public static Project Load(string path)
+    {
+        var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+        var reader = new StreamReader(stream, Encoding.UTF8);
+        return JsonConvert.DeserializeObject<Project>(reader.ReadToEnd());
+    }
+
+    public static void Dump(string path, Project project, bool indented = false)
+    {
+        var stream = new FileStream(path, FileMode.Create, FileAccess.Write);
+        var writer = new StreamWriter(stream, new UTF8Encoding(false));
+        var settings = new JsonSerializerSettings
+        {
+            Formatting = indented ? Formatting.Indented : Formatting.None,
+            ReferenceLoopHandling = ReferenceLoopHandling.Error
+        };
+        writer.Write(JsonConvert.SerializeObject(project, settings));
+        writer.Flush();
+        stream.Flush();
+        writer.Close();
+        stream.Close();
+        writer.Dispose();
+        stream.Dispose();
+    }
+}
