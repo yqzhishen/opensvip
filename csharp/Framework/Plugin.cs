6f280121acdfed8f6824ca76a863cef579a74ac3
yqzhishen <yangqian_1015@icloud.com>
2022-03-14 13:33:07 +0800

    Add attribute "Requirements" to plugin properties

--- csharp/Framework/Plugin.cs
+++ csharp/Framework/Plugin.cs
@@ -9,12 +9,13 @@ namespace OpenSvip.Framework
     {
         public string Name { get; set; }
         public string Version { get; set; }
-        public string Identifier { get; set; }
-        public string Format { get; set; }
-        public string Suffix { get; set; }
-        public string Description { get; set; }
         public string Author { get; set; }
         public string HomePage { get; set; }
+        public string Description { get; set; }
+        public string Requirements { get; set; }
+        public string Format { get; set; }
+        public string Suffix { get; set; }
+        public string Identifier { get; set; }
         public string LibraryPath { get; set; }
         public string Converter { get; set; }
         public Option[] Options { get; set; }
