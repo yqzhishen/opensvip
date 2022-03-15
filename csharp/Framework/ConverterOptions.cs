7d5ff8267e642dd3b4954dae2a58f273d1600057
yqzhishen <yangqian_1015@icloud.com>
2022-03-15 23:30:44 +0800

    添加方法注释

--- csharp/Framework/ConverterOptions.cs
+++ csharp/Framework/ConverterOptions.cs
@@ -6,6 +6,9 @@ using System.Reflection;
 
 namespace OpenSvip.Framework
 {
+    /// <summary>
+    /// 由 OpenSVIP Framework 提供给工程转换器的转换选项。
+    /// </summary>
     public class ConverterOptions
     {
         private readonly Dictionary<string, string> OptionDictionary;
@@ -15,16 +18,31 @@ namespace OpenSvip.Framework
             OptionDictionary = dictionary;
         }
 
+        /// <summary>
+        /// 判断用户是否指定了某个选项。
+        /// </summary>
+        /// <param name="name">选项名称</param>
         public bool ContainsOption(string name)
         {
             return OptionDictionary.ContainsKey(name);
         }
 
+        /// <summary>
+        /// 取出字符串类型的选项值。
+        /// </summary>
+        /// <param name="name">选项名称</param>
+        /// <param name="defaultValue">用户未指定该选项时，返回此默认值</param>
         public string GetOptionAsString(string name, string defaultValue = null)
         {
             return OptionDictionary.ContainsKey(name) ? OptionDictionary[name] : defaultValue;
         }
 
+        /// <summary>
+        /// 取出整数类型的选项值。
+        /// </summary>
+        /// <param name="name">选项名称</param>
+        /// <param name="defaultValue">用户未指定该选项时，返回此默认值</param>
+        /// <exception cref="ArgumentException">若用户输入的选项值不是整数类型</exception>
         public int GetOptionAsInteger(string name, int defaultValue = 0)
         {
             try
@@ -36,7 +54,13 @@ namespace OpenSvip.Framework
                 throw new ArgumentException($"选项 \"{name}\" 格式不合法：应为整数。");
             }
         }
-
+        
+        /// <summary>
+        /// 取出浮点数类型的选项值。
+        /// </summary>
+        /// <param name="name">选项名称</param>
+        /// <param name="defaultValue">用户未指定该选项时，返回此默认值</param>
+        /// <exception cref="ArgumentException">若用户输入的选项值不是浮点数类型</exception>
         public double GetOptionAsDouble(string name, double defaultValue = 0.0)
         {
             try
@@ -49,6 +73,12 @@ namespace OpenSvip.Framework
             }
         }
 
+        /// <summary>
+        /// 取出布尔类型的选项值。
+        /// </summary>
+        /// <param name="name">选项名称</param>
+        /// <param name="defaultValue">用户未指定该选项时，返回此默认值</param>
+        /// <exception cref="ArgumentException">若用户输入的选项值不是布尔类型</exception>
         public bool GetOptionAsBoolean(string name, bool defaultValue = false)
         {
             try
@@ -61,6 +91,13 @@ namespace OpenSvip.Framework
             }
         }
 
+        /// <summary>
+        /// 取出枚举类型的选项值。优先使用枚举变量上注解的 Description 属性与用户输入进行匹配，其次使用枚举变量名称。匹配时忽略大小写。
+        /// </summary>
+        /// <param name="name">选项名称</param>
+        /// <param name="defaultValue">用户未指定该选项时，返回此默认值</param>
+        /// <typeparam name="E">需要返回的枚举变量类型</typeparam>
+        /// <exception cref="ArgumentException">若用户的输入无法匹配任何枚举变量</exception>
         public E GetOptionAsEnum<E>(string name, E defaultValue = default) where E: Enum
         {
             if (!OptionDictionary.ContainsKey(name))
@@ -81,7 +118,7 @@ namespace OpenSvip.Framework
                     return (E) Enum.Parse(enumType, fields[i].Name);
                 }
             }
-            throw new ArgumentException($"选项 \"{name}\" 格式不合法，只能为： {string.Join(" 或 ", tags)}。");
+            throw new ArgumentException($"选项 \"{name}\" 格式不合法：只能为 {string.Join(" 或 ", tags)}。");
         }
     }
 }
