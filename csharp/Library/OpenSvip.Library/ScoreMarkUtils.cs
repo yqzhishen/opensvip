64a49ca5f438cd0a442ae73a421ef38e696e00aa
yqzhishen <yangqian_1015@icloud.com>
2022-03-21 01:16:39 +0800

    工具类添加方法，曲速拍号 skip & shift

--- csharp/Library/OpenSvip.Library/ScoreMarkUtils.cs
+++ csharp/Library/OpenSvip.Library/ScoreMarkUtils.cs
@@ -4,8 +4,16 @@ using OpenSvip.Model;
 
 namespace OpenSvip.Library
 {
+    /// <summary>
+    /// 处理曲速和拍号的工具类。
+    /// </summary>
     public static class ScoreMarkUtils
     {
+        /// <summary>
+        /// 给定曲速列表从前端截去一定长度，并返回以截断处为零点的新曲速列表。
+        /// </summary>
+        /// <param name="tempoList">原曲速列表</param>
+        /// <param name="skipTicks">截断长度（梯）</param>
         public static List<SongTempo> SkipTempoList(List<SongTempo> tempoList, int skipTicks)
         {
             var result = tempoList
@@ -34,6 +42,11 @@ namespace OpenSvip.Library
             return result;
         }
 
+        /// <summary>
+        /// 截断给定拍号列表前面的若干个小节，并返回以截断处为零点的新拍号列表。
+        /// </summary>
+        /// <param name="beatList">原拍号列表</param>
+        /// <param name="skipBars">截断的小节数</param>
         public static List<TimeSignature> SkipBeatList(List<TimeSignature> beatList, int skipBars)
         {
             var result = beatList
@@ -54,6 +67,39 @@ namespace OpenSvip.Library
                     Denominator = beatList[0].Denominator
                 });
             }
+            return result;
+        }
+
+        public static List<SongTempo> ShiftTempoList(List<SongTempo> tempoList, int shiftTicks)
+        {
+            var result = new List<SongTempo>
+            {
+                tempoList[0]
+            };
+            result.AddRange(tempoList
+                .Skip(1)
+                .Select(tempo => new SongTempo
+                {
+                    Position = tempo.Position + shiftTicks,
+                    BPM = tempo.BPM
+                }));
+            return result;
+        }
+
+        public static List<TimeSignature> ShiftBeatList(List<TimeSignature> beatList, int shiftBars)
+        {
+            var result = new List<TimeSignature>
+            {
+                beatList[0]
+            };
+            result.AddRange(beatList
+                .Skip(1)
+                .Select(beat => new TimeSignature
+                {
+                    BarIndex = beat.BarIndex + shiftBars,
+                    Numerator = beat.Numerator,
+                    Denominator = beat.Denominator
+                }));
             return result;
         }
     }
