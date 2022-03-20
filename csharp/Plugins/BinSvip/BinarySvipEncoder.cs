b8591b505b022a9dd76709b536e8c5e848b85b65
yqzhishen <yangqian_1015@icloud.com>
2022-03-20 17:47:25 +0800

    移除不必要的 using

--- csharp/Plugins/BinSvip/BinarySvipEncoder.cs
+++ csharp/Plugins/BinSvip/BinarySvipEncoder.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
-using Newtonsoft.Json;
 using OpenSvip.Const;
-using OpenSvip.Framework;
 using OpenSvip.Library;
 using OpenSvip.Model;
 
