4fb4be1a675f0648570031ed3eecc90d4ecc0087
Fluty Deer <68263367+flutydeer@users.noreply.github.com>
2022-04-15 00:15:11 +0800

    vog插件：支持xs转vog

---
+++ csharp/Plugins/Vogen/VogenEncoder.cs
@@ -0,0 +1,105 @@
+﻿using OpenSvip.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vogen.Model;
+
+namespace Plugin.Vogen
+{
+    public class VogenEncoder
+    {
+        private Project xsProject;
+
+        private VogenProject vogenProject;
+
+        public VogenProject EncodeProject(Project project)
+        {
+            xsProject = project;
+            vogenProject = new VogenProject
+            {
+                BPM = xsProject.SongTempoList[0].BPM,
+                TimeSignature = GetTimeSignature(xsProject.TimeSignatureList[0]),
+                InstrumentalOffset = 0,
+                TrackList = EncodeTrackList()
+            };
+            return vogenProject;
+        }
+
+        private string GetTimeSignature(TimeSignature timeSignature)
+        {
+            return timeSignature.Numerator + "/" + timeSignature.Denominator;
+        }
+
+        private List<VogTrack> EncodeTrackList()
+        {
+            List<VogTrack> vogTrackList = new List<VogTrack>();
+            int trackID = 0;
+            foreach (var track in xsProject.TrackList)
+            {
+                switch (track)
+                {
+                    case SingingTrack singingTrack:
+                        vogTrackList.Add(EncodeSingingTrack(trackID, singingTrack));
+                        trackID++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return vogTrackList;
+        }
+
+        private VogTrack EncodeSingingTrack(int trackID, SingingTrack singingTrack)
+        {
+            VogTrack vogTrack = new VogTrack
+            {
+                TrackName = "utt-" + trackID,
+                SingerName = "Doaz",
+                RomScheme = "man",
+                NoteList = EncodeNoteList(singingTrack.NoteList)
+            };
+            return vogTrack;
+        }
+
+        private List<VogNote> EncodeNoteList(List<Note> noteList)
+        {
+            List<VogNote> vogNoteList = new List<VogNote>();
+            foreach (var note in noteList)
+            {
+                vogNoteList.Add(EncodeNote(note));
+            }
+            return vogNoteList;
+        }
+
+        private VogNote EncodeNote(Note note)
+        {
+            VogNote vogNote = new VogNote
+            {
+                KeyNumber = note.KeyNumber,
+                Lyric = note.Lyric,
+                Pronunciation = GetPronunciation(note),
+                StartPosition = note.StartPos,
+                Duration = note.Length
+            };
+            return vogNote;
+        }
+
+        private string GetPronunciation(Note note)
+        {
+            if (note.Lyric == "-")
+            {
+                return "-";
+            }
+            else if (note.Pronunciation == null)
+            {
+                return "";
+            }
+            else
+            {
+                return note.Pronunciation;
+            }
+        }
+    }
+}
