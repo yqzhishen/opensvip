ff51f57fce44206231db225b6bf6caa1379ef672
Fluty Deer <68263367+flutydeer@users.noreply.github.com>
2022-05-01 00:37:28 +0800

    midi插件：支持带歌词转mid

---
+++ csharp/Plugins/Midi/MidiDecoder.cs
@@ -0,0 +1,21 @@
+﻿using OpenSvip.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Midi
+{
+    public class MidiDecoder
+    {
+        public Project DecodeMidiFile(String path)
+        {
+            var xsProject = new Project
+            {
+
+            };
+            return xsProject;
+        }
+    }
+}
