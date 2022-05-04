b375bc048f6abe7c1d9e3763e065d3dca349171b
Fluty Deer <68263367+flutydeer@users.noreply.github.com>
2022-05-04 17:49:06 +0800

    midi插件：添加注释以便开发和维护，小幅重构，防止音符的音高出界。

--- csharp/Plugins/Midi/MidiEncoder.cs
+++ csharp/Plugins/Midi/MidiEncoder.cs
@@ -8,34 +8,53 @@ using Melanchall.DryWetMidi.Interaction;
 using Melanchall.DryWetMidi.MusicTheory;
 using OpenSvip.Framework;
 using OpenSvip.Model;
-using MidiNote = Melanchall.DryWetMidi.Interaction.Note;
-using OsNote = OpenSvip.Model.Note;
+using Note = OpenSvip.Model.Note;
 using NPinyin;
 
 namespace Plugin.Midi
 {
     public class MidiEncoder
     {
+        /// <summary>
+        /// 移调。
+        /// </summary>
         public int Transpose { get; set; }
 
+        /// <summary>
+        /// 是否使用歌词兼容性模式，默认为否。
+        /// </summary>
         public bool IsUseCompatibleLyric { get; set; }
-        
+
+        /// <summary>
+        /// 是否移除歌词中的常见标点符号，默认为是。
+        /// </summary>
         public bool IsRemoveSymbols { get; set; }
 
+        /// <summary>
+        /// 歌词文本编码。
+        /// </summary>
         public LyricEncodings LyricEncoding { get; set; }
 
+        /// <summary>
+        /// 时基，默认为480。
+        /// </summary>
         public int PPQ { get; set; }
 
         private Project osProject;
 
         private MidiFile midiFile = new MidiFile();
 
+        /// <summary>
+        /// 转换为 MIDI 文件。
+        /// </summary>
+        /// <param name="project">原始的 OpenSvip 工程。</param>
+        /// <param name="path">输出路径。</param>
         public void EncodeMidiFile(Project project, string path)
         {
             osProject = project;
             TicksPerQuarterNoteTimeDivision timeDivision = new TicksPerQuarterNoteTimeDivision((short)PPQ);
-            midiFile.TimeDivision = timeDivision;
-            EncodeMidiChunks();
+            midiFile.TimeDivision = timeDivision;//设置时基。
+            EncodeMidiChunks();//相当于 MIDI 里面的轨道。
             WritingSettings settings = new WritingSettings
             {
                 TextEncoding = GetEncoding()
@@ -43,6 +62,9 @@ namespace Plugin.Midi
             midiFile.Write(path, true, settings: settings);
         }
 
+        /// <summary>
+        /// 生成 MIDI Chunks。
+        /// </summary>
         private void EncodeMidiChunks()
         {
             foreach (var trackChunk in EncodeTrackChunkList())
@@ -51,6 +73,10 @@ namespace Plugin.Midi
             }
         }
 
+        /// <summary>
+        /// 将选项转换为编码。
+        /// </summary>
+        /// <returns>文本编码。</returns>
         private Encoding GetEncoding()
         {
             switch (LyricEncoding)
@@ -70,22 +96,26 @@ namespace Plugin.Midi
                 case LyricEncodings.UTF8BOM:
                     return Encoding.UTF8;
                 case LyricEncodings.UTF8:
-                    return new UTF8Encoding(false);
+                    return new UTF8Encoding(false);//不带BOM的UTF-8
                 default:
                     return Encoding.UTF8;
             }
         }
 
+        /// <summary>
+        /// 生成 MIDI Track Chuck 列表
+        /// </summary>
+        /// <returns>含有曲速轨道和演唱轨的 Track Chuck 列表。</returns>
         private List<TrackChunk> EncodeTrackChunkList()
         {
             List<TrackChunk> trackChunkList = new List<TrackChunk>();
-            trackChunkList.Add(EncodeTempoTrackChunk());
+            trackChunkList.Add(EncodeTempoTrackChunk());//MIDI 里的曲速轨。
             foreach (var track in osProject.TrackList)
             {
                 switch (track)
                 {
                     case SingingTrack singingTrack:
-                        trackChunkList.Add(EncodeTrackChunk(singingTrack));
+                        trackChunkList.Add(EncodeNoteTrackChunk(singingTrack));
                         break;
                     default:
                         break;
@@ -94,11 +124,19 @@ namespace Plugin.Midi
             return trackChunkList;
         }
 
+        /// <summary>
+        /// 生成曲速轨。
+        /// </summary>
+        /// <returns>含有曲速事件数组的 Track Chunk。</returns>
         private TrackChunk EncodeTempoTrackChunk()
         {
             return new TrackChunk(EncodeSetTemopEventArray());
         }
 
+        /// <summary>
+        /// 将曲速标记列表转换为 MIDI 事件数组。
+        /// </summary>
+        /// <returns>含有曲速事件的 MIDI Event 数组。</returns>
         private MidiEvent[] EncodeSetTemopEventArray()
         {
             List<MidiEvent> midiEventList = new List<MidiEvent>();
@@ -111,6 +149,12 @@ namespace Plugin.Midi
             return midiEventList.ToArray();
         }
 
+        /// <summary>
+        /// 将单个曲速标记转换为设置曲速 MIDI 事件。
+        /// </summary>
+        /// <param name="tempo">曲速。</param>
+        /// <param name="lastEventAbsoluteTime">上一个曲速事件的绝对时间，单位为梯。</param>
+        /// <returns>设置曲速 MIDI 事件。</returns>
         private SetTempoEvent EncodeSetTempoEvent(SongTempo tempo, ref int lastEventAbsoluteTime)
         {
             SetTempoEvent setTempoEvent = new SetTempoEvent
@@ -121,40 +165,61 @@ namespace Plugin.Midi
             lastEventAbsoluteTime = tempo.Position;
             return setTempoEvent;
         }
-
-        private TrackChunk EncodeTrackChunk(SingingTrack singingTrack)
+        
+        /// <summary>
+        /// 转换演唱轨。
+        /// </summary>
+        /// <param name="singingTrack">原始演唱轨。</param>
+        /// <returns>含有音符事件数组的 Track Chunk。</returns>
+        private TrackChunk EncodeNoteTrackChunk(SingingTrack singingTrack)
         {
             MidiEvent[] midiEvents = EncodeNoteEventArray(singingTrack);
-
             TrackChunk trackChunk = new TrackChunk(midiEvents);
 
             return trackChunk;
         }
 
+        /// <summary>
+        /// 转换音符列表。
+        /// </summary>
+        /// <param name="singingTrack">原始演唱轨。</param>
+        /// <returns>带词音符的 MIDI Event 数组。</returns>
         private MidiEvent[] EncodeNoteEventArray(SingingTrack singingTrack)
         {
             List<MidiEvent> midiEventList = new List<MidiEvent>();
             int lastEventAbsoluteTime = 0;
+            //这里不能用 DryWetMidi 的音符管理器来转换音符，因为不能设置音符的歌词。需要手动生成歌词、音符按下和松开三个事件。
             foreach (var note in singingTrack.NoteList)
             {
-                midiEventList.Add(EncodeLyricEvent(note, lastEventAbsoluteTime));
+                midiEventList.Add(EncodeLyricEvent(note, lastEventAbsoluteTime));//歌词事件设置在音符按下事件的前面。
                 midiEventList.Add(EncodeNoteOnEvent(note));
                 midiEventList.Add(EncodeNoteOffEvent(note, ref lastEventAbsoluteTime));
             }
             return midiEventList.ToArray();
         }
         
-        private LyricEvent EncodeLyricEvent(OsNote note, int lastEventAbsoluteTime)
+        /// <summary>
+        /// 将歌词转换成 MIDI 事件。
+        /// </summary>
+        /// <param name="note">音符。</param>
+        /// <param name="lastEventAbsoluteTime">上一个 MIDI 事件的绝对时间。</param>
+        /// <returns>歌词事件。</returns>
+        private LyricEvent EncodeLyricEvent(Note note, int lastEventAbsoluteTime)
         {
             LyricEvent lyricEvent = new LyricEvent
             {
                 Text = GetLyric(note),
-                DeltaTime = note.StartPos - lastEventAbsoluteTime
+                DeltaTime = note.StartPos - lastEventAbsoluteTime//歌词事件支撑起乐谱中无音符的空白部分，防止所有音符粘连。
             };
             return lyricEvent;
         }
 
-        private string GetLyric(OsNote note)
+        /// <summary>
+        /// 根据输出选项转换歌词。
+        /// </summary>
+        /// <param name="note">音符。</param>
+        /// <returns></returns>
+        private string GetLyric(Note note)
         {
             string lyric;
             if (note.Pronunciation != null)
@@ -179,22 +244,33 @@ namespace Plugin.Midi
             return lyric;
         }
 
-        private NoteOnEvent EncodeNoteOnEvent(OsNote note)
+        /// <summary>
+        /// 生成 Note On 事件。
+        /// </summary>
+        /// <param name="note">音符。</param>
+        /// <returns>Note On 事件。</returns>
+        private NoteOnEvent EncodeNoteOnEvent(Note note)
         {
             NoteOnEvent noteOnEvent = new NoteOnEvent
             {
-                NoteNumber = (SevenBitNumber)(note.KeyNumber + Transpose),
+                NoteNumber = (SevenBitNumber)GetTransposedKeyNumber(note),
                 Velocity = (SevenBitNumber)45,
                 Channel = (FourBitNumber)0
             };
             return noteOnEvent;
         }
 
-        private NoteOffEvent EncodeNoteOffEvent(OsNote note, ref int lastEventAbsoluteTime)
+        /// <summary>
+        /// 生成 Note Off 事件。
+        /// </summary>
+        /// <param name="note">音符。</param>
+        /// <param name="lastEventAbsoluteTime">上一个 MIDI 事件的绝对时间。</param>
+        /// <returns> Note Off 事件。</returns>
+        private NoteOffEvent EncodeNoteOffEvent(Note note, ref int lastEventAbsoluteTime)
         {
             NoteOffEvent noteOffEvent = new NoteOffEvent
             {
-                NoteNumber = (SevenBitNumber)(note.KeyNumber + Transpose),
+                NoteNumber = (SevenBitNumber)GetTransposedKeyNumber(note),
                 Velocity = (SevenBitNumber)0,
                 DeltaTime = note.Length,
                 Channel = (FourBitNumber)0
@@ -203,6 +279,30 @@ namespace Plugin.Midi
             return noteOffEvent;
         }
 
+        /// <summary>
+        /// 获取移调后的音高。
+        /// </summary>
+        /// <param name="note">音符。</param>
+        /// <returns>移调后的音高。</returns>
+        private int GetTransposedKeyNumber(Note note)
+        {
+            int transposedKeyNumber = note.KeyNumber + Transpose;
+            if (transposedKeyNumber < 0)
+            {
+                transposedKeyNumber = 0;
+            }
+            else if (transposedKeyNumber > 127)
+            {
+                transposedKeyNumber = 127;
+            }
+            return transposedKeyNumber;
+        }
+
+        /// <summary>
+        /// 将曲速转换为每四分音符的微秒数。
+        /// </summary>
+        /// <param name="BPM">曲速。</param>
+        /// <returns>每四分音符的微秒数。</returns>
         private long GetMicrosecondsPerQuarterNote(long BPM)
         {
             return (long)(60.0 / BPM * 1000000.0);
