7e22533ac43b7f12a6c38a3527922343880872e4
Fluty Deer <68263367+flutydeer@users.noreply.github.com>
2022-05-23 23:10:35 +0800

    midi插件：初步实现midi转中介模型，新增测试类

---
+++ csharp/Plugins/Midi/Test.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using OpenSvip.Framework;
+using OpenSvip.Model;
+using Midi.Stream;
+
+namespace Plugin.Midi
+{
+    public static class Test
+    {
+        public static void Main(string[] args)
+        {
+            Midi2Json();
+        }
+
+        private static void Midi2Json()
+        {
+            var project = new MidiConverter().Load(@"E:\编曲学习\破云来\破云来带词midi.mid", new ConverterOptions(new Dictionary<string, string>()));
+            var stream = new FileStream(
+                @"E:\编曲学习\破云来\破云来带词midi.json",
+                FileMode.Create,
+                FileAccess.Write);
+            var writer = new StreamWriter(stream, new UTF8Encoding(false));
+            writer.Write(JsonConvert.SerializeObject(project));
+            writer.Flush();
+            stream.Flush();
+            writer.Close();
+            stream.Close();
+        }
+        private static void Json2Midi()
+        {
+            var stream = new FileStream(
+                @"E:\编曲学习\破云来\破云来带词midi.json",
+                FileMode.Open,
+                FileAccess.Read);
+            var reader = new StreamReader(stream, Encoding.UTF8);
+            var project = JsonConvert.DeserializeObject<Project>(reader.ReadToEnd());
+            stream.Close();
+            reader.Close();
+            new MidiConverter().Save(@"E:\编曲学习\破云来\破云来带词midi.mid", project, new ConverterOptions(new Dictionary<string, string>()));
+        }
+    }
+}
